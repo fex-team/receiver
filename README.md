99b933ab0a100fe955554233850ff03bb7047e82
xiangshouding <fansekey@gmail.com>
2014-06-17 22:17:35 +0800

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ receiver
 ========
 
 FIS receiver on nodejs
+
+### use
+
+```bash
+$ git clone https://github.com/fex-team/receiver.git
+$ cd receiver
+$ node server.js # default port 8999, use `node server.js <port>` change port
+```
+
+_fis-conf.js_
+
+```javascript
+fis.config.merge({
+    deploy: {
+        remote: {
+            receiver: 'http://<host>:8999/receiver',
+            from: '/public',
+            //远端目录
+            to: '/home/fis/www/'
+        }
+    }
+});
+```
---
+++ server.js
@@ -0,0 +1,70 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var PORT = parseInt(process.argv[2]) || 8999;
+
+var server = http.createServer(function (req, res) {
+    
+    function error(err) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end(err.toString()); //fail
+    }
+
+    function next(from, to) {
+        fs.readFile(from, function (err, content) {
+            if (err) {
+                eror(err);
+            } else {
+                fs.writeFile(to, content, function (err) {
+                    if (err) {
+                        error(err);
+                    }
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.end('0'); //success
+                });
+            }
+        });
+    }
+
+    if (req.url == '/') {
+        // show a file upload form
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end('I\'m ready for that, you know.');
+    } else if (req.url == '/receiver' && req.method.toLowerCase() == 'post') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            if (err) {
+                error(err);
+            } else {
+                var to = fields['to'];
+                fs.exists(to, function (exists) {
+                    if (exists) {
+                        fs.unlink(to, function (err) {
+                            next(files.file.path, to); 
+                        });
+                    } else {
+                        fs.exists(path.dirname(to), function (exists) {
+                            if (exists) {
+                                next(files.file.path, to); 
+                            } else {
+                                mkdirp(path.dirname(to), 0777, function (err) {
+                                    if (err) {
+                                        error(err);
+                                        return;
+                                    }
+                                    next(files.file.path, to); 
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
+
+server.listen(PORT, function () {
+    console.log('receiver listening *:' + PORT);
+});
